8aeaf822d462d751453621c11ea1ea10f1b4ac46
heiwa4126 <heiwa4126@gmail.com>
2022-09-27 17:05:11 +0900

    (snap)

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -4,10 +4,13 @@ package com.example.app;
  *
  */
 import java.security.MessageDigest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App
 {
     private static void test1() {
+        // https://kaworu.jpn.org/java/Java%E3%81%A7SHA256%E3%82%92%E8%A8%88%E7%AE%97%E3%81%99%E3%82%8B
         String text = "hoge";
         byte[] cipher_byte;
         try{
@@ -24,9 +27,22 @@ public class App
         }
     }
 
-    public static void main( String[] args )
+    public static void test2() throws JsonProcessingException {
+        // https://qiita.com/opengl-8080/items/b613b9b3bc5d796c840c
+        Hoge hoge = new Hoge();
+        hoge.id = 10;
+        hoge.name = "hoge";
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(hoge);
+
+        System.out.println(json);
+    }
+
+    public static void main( String[] args ) throws JsonProcessingException
     {
         System.out.println( "Hello World!" );
         test1();
+        test2();
     }
 }
---
+++ src/main/java/com/example/app/Hoge.java
@@ -0,0 +1,12 @@
+package com.example.app;
+// https://qiita.com/opengl-8080/items/b613b9b3bc5d796c840c
+
+public class Hoge {
+    public int id;
+    public String name;
+
+    @Override
+    public String toString() {
+        return "Hoge [id=" + id + ", name=" + name + "]";
+    }
+}

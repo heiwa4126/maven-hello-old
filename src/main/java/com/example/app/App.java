072630f9b0aa9d547cab6d38c84bb7e571fe48bc
heiwa4126 <heiwa4126@gmail.com>
2022-09-30 10:26:55 +0900

    (snap)

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -2,22 +2,28 @@ package com.example.app;
 
 /**
  * Hello world! and something
+ * @author me
+ * @version 1.0
  */
 import java.security.MessageDigest;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
+    /**
+     * main class
+     */
     private static void test1() {
         // https://kaworu.jpn.org/java/Java%E3%81%A7SHA256%E3%82%92%E8%A8%88%E7%AE%97%E3%81%99%E3%82%8B
-        String text = "hoge";
+        var text = "hoge";
         byte[] cipher_byte;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            var md = MessageDigest.getInstance("SHA-256");
             md.update(text.getBytes());
             cipher_byte = md.digest();
-            StringBuilder sb = new StringBuilder(2 * cipher_byte.length);
-            for (byte b : cipher_byte) {
+            var sb = new StringBuilder(2 * cipher_byte.length);
+            for (final byte b : cipher_byte) {
                 sb.append(String.format("%02x", b & 0xff));
             }
             System.out.println(sb);
@@ -38,6 +44,12 @@ public class App {
         System.out.println(json);
     }
 
+    /**
+     * main entry
+     *
+     * @param args
+     * @throws JsonProcessingException
+     */
     public static void main(String[] args) throws JsonProcessingException {
         System.out.println("Hello World!");
         test1();
